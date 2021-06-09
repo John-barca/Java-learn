f30c67fb1cc486af8ac06c1a97580cc0baae986b
John-barca <1113903029@qq.com>
2021-06-09 11:54:39 +0800

    项目初始化

---
+++ my-social/haodong/src/main/java/com/example/haodong/HaodongApplication.java
@@ -0,0 +1,11 @@
+package com.example.haodong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaodongApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HaodongApplication.class, args);
+	}
+}
---
+++ my-social/haodong/src/test/java/com/example/haodong/HaodongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haodong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaodongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
